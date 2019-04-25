d24cd8eca65ee09e70db9cea899d93f2b483c526
rafamorais <rafaelsilvademorais@gmail.com>
2019-04-25 19:00:34 -0300

    Alterando migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_011621) do
+ActiveRecord::Schema.define(version: 2019_04_20_013313) do
+
+  create_table "animals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "peso"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.integer "age"
+    t.integer "peso"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
